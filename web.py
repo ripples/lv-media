713cdfb7c3d0e8386b07731a13e4867d2da4ba95
cheyans <cheyan.set@gmail.com>
2016-05-30 02:31:04 -0400

    dockerize

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+import os
 import sys
 import json
 from flask import Flask, abort
@@ -9,32 +10,46 @@ import media
 mediaobj = None
 
 # Create the Flask application
-app      = Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/favicon.ico')
 def index():
     abort(404)
 
+
 @app.route('/semesters')
 def semesters():
     return json.dumps(mediaobj.semesters())
+
 
 @app.route('/<semester>')
 def courses(semester):
     return json.dumps(mediaobj.courses(semester))
 
+
 @app.route('/<semester>/<course>')
 def lectures(semester, course):
     return json.dumps(mediaobj.lectures(semester, course))
+
 
 @app.route('/<semester>/<course>/<lecture>')
 def lecture(semester, course, lecture):
     return json.dumps(mediaobj.lecture(semester, course, lecture))
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('usage: {} MEDIADIR'.format(sys.argv[0]))
+    if len(sys.argv) == 2:
+        media_dir = sys.argv[1]
+    elif 'MEDIA_DIR' in os.environ:
+        media_dir = os.environ['MEDIA_DIR']
+    else:
+        print('usage: {} MEDIA_DIR or create the appropriate system variable'.format(sys.argv[0]))
         sys.exit()
-    mediaobj  = media.make(sys.argv[1])
+
+    mediaobj = media.make(media_dir)
+    port = 5000
+    if 'MEDIA_SERVER_PORT' in os.environ:
+        port = os.environ['MEDIA_SERVER_PORT']
+
     app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=port)
